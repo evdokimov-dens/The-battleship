e908f3e301a49cf94539f729de2722af494fc26e
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-12-02 00:35:20 +0500

    add:  new method fire and isSunk

--- js/main.js
+++ js/main.js
@@ -7,5 +7,54 @@ let view = {
     let cell = document.getElementById(location);
     cell.setAttribute("class", "hit");
   },
-  displayMiss: function (location) {},
+  displayMiss: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
 };
+
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },
+  ],
+  fire: function (guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
+};
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
